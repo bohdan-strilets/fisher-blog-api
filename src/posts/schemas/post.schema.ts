6491ea63dda7b1654a7225d3082cadf7ba197859
Bohdan Strilets <bohdan.strilets@gmail.com>
2023-06-14 15:57:54 +0200

    Changed post schema.

--- src/posts/schemas/post.schema.ts
+++ src/posts/schemas/post.schema.ts
@@ -3,7 +3,6 @@ import mongoose, { HydratedDocument, Types } from 'mongoose';
 import { User } from 'src/users/schemas/user.schema';
 import { PostBodyType } from '../types/post-body.type';
 import { Statistics, StatisticsDocument } from './statistics.schema';
-import { CommentType } from '../types/comment.type';
 
 export type PostDocument = HydratedDocument<Post>;
 
@@ -26,9 +25,6 @@ export class Post {
 
   @Prop({ type: Statistics, default: {} })
   statistics: StatisticsDocument;
-
-  @Prop({ default: [] })
-  comments: CommentType[];
 
   @Prop({ required: true })
   posterURL: string;
